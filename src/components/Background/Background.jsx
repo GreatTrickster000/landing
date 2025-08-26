8f6f4ff2ae7432cc9d08609d009f12166b70b9bf
Yehor Tsekhovskyi <egor.tsehovskoy@gmail.com>
2025-08-26 12:56:18 +0200

    chore: empty baseline for review (shared history)

--- src/components/Background/Background.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Background.module.scss";
-
-function Background({ children }) {
-  return <div className={styles.background}>{children}</div>;
-}
-
-export default Background;
--- src/components/Navigation/Navigation.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Navigation.module.scss";
-
-function Nav({ children, size }) {
-  return (
-    <nav className={`${styles.nav} ${styles[`nav--${size}`]}`}>{children}</nav>
-  );
-}
-
-export default Nav;
